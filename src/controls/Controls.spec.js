1d17644f6cc227f23af273ba9d261751cde76b4a
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-12-16 15:27:05 +0000

    gate, dashboard and control tests all functioning

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,41 @@
-// Test away!
+// // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+// ### Controls Component
+
+// - provide buttons to toggle the `closed` and `locked` states.
+// - buttons' text changes to reflect the state the door will be in if clicked
+// - the closed toggle button is disabled if the gate is locked
+// - the locked toggle button is disabled if the gate is open
+
+afterEach(rtl.cleanup);
+
+describe("Controls", () => {
+  it("provides button toggle closed state", () => {
+    const wrapper = rtl.render(<Controls />);
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  });
+  it("provides button toggle locked state", () => {
+    const wrapper = rtl.render(<Controls />);
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+  });
+  it("reflect button text change on clicking unlock", () => {
+    const wrapper = rtl.render(<Controls locked={true} />);
+    expect(wrapper.queryByText(/unlock gate/i)).toBeInTheDocument();
+  });
+  it("reflect button text change on clicking closebtn", () => {
+    const wrapper = rtl.render(<Controls closed={true} />);
+    expect(wrapper.queryByText(/open gate/i)).toBeInTheDocument();
+  });
+  it("toggle button disabled when locked", () => {
+    const wrapper = rtl.render(<Controls locked={true} />);
+    expect(wrapper.queryByText(/close gate/i)).toHaveAttribute("disabled");
+  });
+  it("toggle button is disabled when gate is open", () => {
+    const wrapper = rtl.render(<Controls closed={false} />);
+    expect(wrapper.queryByText(/lock gate/i)).toHaveAttribute("disabled");
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,33 @@
 // Test away
+import Dashboard from "./Dashboard";
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+describe("display component", () => {
+  test("shows controls and initial display", () => {
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  });
+
+  test("shows default values - unlocked and open", () => {
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+  });
+
+  test("cannot be closed or opened if locked", () => {
+    rtl.fireEvent.click(wrapper.queryByText(/lock gate/i));
+    expect(wrapper.queryByText(/open gate/i)).not.toBeInTheDocument;
+    expect(wrapper.queryByText(/close gate/i)).not.toBeInTheDocument;
+  });
+});
